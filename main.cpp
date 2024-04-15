f32f5dd4fb0c0eb98aa92eb327912de16b4dc3cd
Alio <119178113+Alverot@users.noreply.github.com>
2024-04-15 12:33:54 +0300

    Start.1
    created the program in Clion

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    int da;
+    cout<<"Introduceti numarul :";
+    cin >> da;
+    printf("Numarul este %d",da);
+
+
+
+
+
+    return 0;
+}
